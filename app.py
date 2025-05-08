cf1f7d01e5484f568a913a3fd29e4b78f1ec51c3
Jason Huang <23062@burnside.school.nz>
2025-05-09 11:43:14 +1200

    added sort gpus by price option

--- app.py
+++ app.py
@@ -14,11 +14,45 @@ def print_all_gpu():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop thorugh all the results
-    print("name                          price memory  speed year manufacturer_id")
+    print(f"name                          price memory  speed year manufacturer_id")
     for gpu in results:
         print(f"{gpu[1]:<30}{gpu[2]:<8}{gpu[3]:<6}{gpu[4]:<6}{gpu[5]:<6}{gpu[6]:<6}")
     #loop finished here
     db.close()
 
+
+
+def print_all_gpu_by_price():
+    '''print all the GPUs sorted by price'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from gpu ORDER BY price DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop thorugh all the results
+    print(f"name                          price memory  speed year manufacturer_id")
+    for gpu in results:
+        print(f"{gpu[1]:<30}{gpu[2]:<8}{gpu[3]:<6}{gpu[4]:<6}{gpu[5]:<6}{gpu[6]:<6}")
+    #loop finished here
+    db.close()
+
+
+
 #main code
-print_all_gpu()
+while True:
+    user_input = input(
+"""
+What would you like to do.
+1.Print all GPUs
+2.Print all GPUs sorted by price
+3.Exit
+
+""")
+    if user_input == "1":
+        print_all_gpu()
+    elif user_input == "2":
+        print_all_gpu_by_price()
+    elif user_input == "3":
+        break
+    else:
+        print("That was not an option\n")
