dec35466705b4774ace51ad65fb4c1013cf481d5
Jason Huang <23062@burnside.school.nz>
2025-05-08 14:35:23 +1200

    tested database

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello world")
+import sqlite3
+
+db = sqlite3.connect(GPU.db)
+cursor = db.cursor()
+sql = "SELECT * from gpu;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+
+
+db.close()
